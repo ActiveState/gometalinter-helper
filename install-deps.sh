17dac320637b5c8b08b8ac9fdfa2c123d38e1d37
Dave Rolsky <autarch@urth.org>
2018-03-07 14:17:47 -0600

    Update circleci config for new command name

--- install-deps.sh
+++ install-deps.sh
@@ -2,7 +2,11 @@
 
 set -eo pipefail
 
-go get -u github.com/golang/lint/golint
+go get -u gopkg.in/alecthomas/gometalinter.v2
+# includes aligncheck, structcheck, & varcheck
+go get -u github.com/opennota/check
 go get -u github.com/kisielk/errcheck
-go get -u github.com/golang/dep/cmd/dep
+go get -u github.com/golang/lint/golint
+go get -u honnef.co/go/tools/cmd/megacheck
+go get -u github.com/client9/misspell/cmd/misspell
 
