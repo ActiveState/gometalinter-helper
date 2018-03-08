18e943973cef8e56c432d8b91850ff89ee793b13
Dave Rolsky <autarch@urth.org>
2018-03-08 10:54:18 -0600

    Fix self-check in circleci so it actually works

--- install-deps.sh
+++ install-deps.sh
@@ -3,10 +3,10 @@
 set -eo pipefail
 
 go get -u gopkg.in/alecthomas/gometalinter.v2
-# includes aligncheck, structcheck, & varcheck
-go get -u github.com/opennota/check
-go get -u github.com/kisielk/errcheck
-go get -u github.com/golang/lint/golint
-go get -u honnef.co/go/tools/cmd/megacheck
-go get -u github.com/client9/misspell/cmd/misspell
 
+go get -u github.com/client9/misspell/cmd/misspell
+go get -u github.com/golang/lint/golint
+go get -u github.com/kisielk/errcheck
+go get -u github.com/opennota/check/cmd/structcheck
+go get -u github.com/tsenart/deadcode
+go get -u honnef.co/go/tools/cmd/megacheck
---
+++ self-check.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eo pipefail
+
+command="go run ./cmd/gometalinter-helper/main.go"
+if [ "$@" ]; then
+    command=$@
+fi
+$command \
+    -verbose \
+    -exe gometalinter.v2 \
+    -- \
+    --vendor \
+    --disable-all \
+    --sort=path \
+    --enable=deadcode \
+    --enable=errcheck \
+    --enable=gofmt \
+    --enable=golint \
+    --enable=megacheck \
+    --enable=misspell \
+    --enable=structcheck \
+    ./...
